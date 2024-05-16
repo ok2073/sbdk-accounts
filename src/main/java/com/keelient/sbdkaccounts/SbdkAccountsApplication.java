d87288860f22be0a81e3d70901872e5902812d6d
ok2073 <ok2073@proton.me>
2024-05-16 15:40:48 +0200

    first commit

---
+++ src/main/java/com/keelient/sbdkaccounts/SbdkAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.keelient.sbdkaccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbdkAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbdkAccountsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/keelient/sbdkaccounts/SbdkAccountsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.keelient.sbdkaccounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbdkAccountsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
